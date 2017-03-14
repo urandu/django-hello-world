32779e55cd136985074b85373735f532beca463d
urandu <bildad.urandu@jumo.world>
2017-03-14 17:17:59 +0300

    views done!

--- website/urls.py
+++ website/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from app.views import hello
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^hello/', hello, name='hello'),
 ]
